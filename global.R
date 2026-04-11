38eb8bce9a4fff2328e03aa2401e62cac55129a2
s3rgiorafael2018-create <s3rgiorafael2018@gmail.com>
2026-04-11 08:07:08 -0500

    Cambio del Global.R para una buena ruta del archivo csv principal

--- global.R
+++ global.R
@@ -9,7 +9,7 @@ library(randomForest)
 library(nortest)
 
 #Carga del dataset
-datos<-read.csv("data_payments.csv")
+datos<-read.csv("data_payments.txt", stringsAsFactors = FALSE)
 #Se cambia el nombre de las variables para mejor claridad
 datos %>% 
   select(clave=KEY, frecuencia=FREQ, pais_origen=REF_AREA, pais_destino=COUNT_AREA,
